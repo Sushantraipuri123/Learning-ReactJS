2291109ba434a6461e49ddccccc5b268440b5f97
Sushant <cashu853@gmail.com>
2024-03-07 12:31:39 +0530

    some changes

--- ShowHidePassword/ShowHide/src/App.jsx
+++ ShowHidePassword/ShowHide/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
 function App() {
   const [pass, setPass] = useState(false)
 
@@ -8,14 +9,20 @@ function App() {
     <>
    <h1 className=' bg-slate-500 mb-9'>Show Hide Password</h1>
 
+   <div className=' bg-blue-200 inline  px-3 py-3 rounded-lg'>
+
+
    <input type={pass ? "text":"password"}
-   className=' bg-blue-200 px-3 py-1 rounded-full focus:outline-none'
+   className=' bg-blue-200 px-3 py-1 focus:outline-none'
    placeholder='Your pass'
    />
 
    <button
    onClick={()=>setPass(!pass)}
-   >{pass?"":""}</button>
+   >{pass?<FaEye />:<FaEyeSlash />}</button>
+
+   </div>
+
     </>
   )
 }
