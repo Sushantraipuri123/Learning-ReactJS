03ba9663d4d281cadd8550c2ce67482fdd695e60
Sushant <cashu853@gmail.com>
2024-03-16 22:03:18 +0530

    learning Flowbite

---
+++ flowbitreact/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+<h1>
+Learning  flowbite 
+
+</h1>
+
+   </>
+  )
+}
+
+export default App
