126c4325aa9509d0bf45cfde715663f207f0df30
Sushant <cashu853@gmail.com>
2024-03-17 14:20:32 +0530

    Learning Flowbite a UI Library

--- flowbitreact/src/App.jsx
+++ flowbitreact/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from 'flowbite-react';
 import './App.css'
+import { Card } from 'flowbite-react';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +10,20 @@ function App() {
    <>
 <h1 className=' bg-slate-400 text-3xl'> 
 Learning  flowbite 
-
 </h1>
+
+<Card href="#" className="max-w-sm mt-10">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+<Button outline gradientDuoTone="purpleToBlue" className=' mt-7 focus:outline-none'>
+        Purple to Blue
+      </Button>
+    </Card>
+
 
    </>
   )
