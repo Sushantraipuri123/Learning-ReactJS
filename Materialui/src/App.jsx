fe306083ffbed040b9efb33649636bcce7835167
Sushant <cashu853@gmail.com>
2024-03-19 22:09:11 +0530

    Learning Material-UI

---
+++ Materialui/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@mui/material'
+
+
+function App() {
+   
+
+  return (
+    <>
+     <Button variant='contained'>Login</Button>
+    </>
+  )
+}
+
+export default App
