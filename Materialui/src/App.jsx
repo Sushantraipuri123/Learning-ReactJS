eb3fce106db575c9b85a07a211bcf870342e3180
Sushant <cashu853@gmail.com>
2024-04-02 11:13:10 +0530

    Learning MUI

--- Materialui/src/App.jsx
+++ Materialui/src/App.jsx
@@ -1,16 +1,14 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import { Container } from '@mui/material'
 
 
 function App() {
    
 
-  return (
-    <>
-    <Navbar/>
-     <Home/>
-    </>
-  )
+  return <Container>
+    Hello World
+  </Container>
 }
 
 export default App
