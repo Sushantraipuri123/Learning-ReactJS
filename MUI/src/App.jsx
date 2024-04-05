aef7fda3eca92b2fb179a470bef90df436293ec0
Sushant <cashu853@gmail.com>
2024-04-05 11:53:06 +0530

    Learning MUI

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Checkbox from '@mui/material/Checkbox';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Floatbtn from './components/Floatbtn';
 
 function App() {
   const [color, setColor] = useState("secondary")
   const [disable, setDisable] = useState(false)
+  const [includes, setIncludes] = useState(true)
+  const [likes, setLikes] = useState(0)
 
   let customBtn = ()=>{
      alert("I Was Clicked")
      setDisable(true)
   }
+
+  const handleLikes = () => {
+    setLikes(likes === 0 ? 1 : 0);
+  };
 
   return (
     <>
@@ -23,6 +32,25 @@ function App() {
        disabled={disable}
        >MUI button</Button>
        
+       <h1>MUI Button Group</h1>
+
+       <ButtonGroup variant='contained' orientation='horizontal'>
+       <Button>one </Button>
+       <Button>two </Button>
+       <Button>three</Button>
+
+       </ButtonGroup>
+
+       <h1>MUI Check box</h1>
+       <Checkbox color='secondary' value={includes} disableRipple onClick={()=>{console.log(includes)}} onChange={()=>{setIncludes(!includes)}}/>
+
+       <h1>Like Dislike Check box</h1>
+       <Checkbox  color='secondary'   onChange={handleLikes} icon={<BookmarkBorderIcon  />} checkedIcon={<BookmarkIcon  />} /> {likes}
+         
+         <div className='floatbtn'>
+
+       <Floatbtn/>
+         </div>
     </>
   )
 }
---
+++ MUI/src/components/Floatbtn.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Fab from '@mui/material/Fab';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
+export default function Floatbtn() {
+  return (
+  <div>
+      <Fab variant="extended" color="primary">
+        <NavigationIcon />
+      </Fab>
+      </div>
+  );
+}
