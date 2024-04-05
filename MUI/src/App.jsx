ae2e2defbaa65ae1f1e5f3567c948b1655391c73
Sushant <cashu853@gmail.com>
2024-04-05 14:28:25 +0530

    Learning MUI

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -6,12 +6,25 @@ import Checkbox from '@mui/material/Checkbox';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Floatbtn from './components/Floatbtn';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { pink } from '@mui/material/colors';
 
 function App() {
   const [color, setColor] = useState("secondary")
   const [disable, setDisable] = useState(false)
   const [includes, setIncludes] = useState(true)
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('');
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    alert(value)
+  };
 
   let customBtn = ()=>{
      alert("I Was Clicked")
@@ -51,6 +64,24 @@ function App() {
 
        <Floatbtn/>
          </div>
+
+         
+       <h1>MUI radio btn</h1>
+
+       <FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        value={value}
+        onChange={handleChange}
+      >
+        <FormControlLabel value="male" color='secondary'  control={<Radio  disableRipple color='secondary'/>} label="Female"/>
+        <FormControlLabel value="female" color='secondary' control={<Radio  disableRipple  sx={{color:pink[800], '&.Mui-checked': {
+            color: pink[600],
+          },}}/>} label="Male" />
+      </RadioGroup>
+    </FormControl>
     </>
   )
 }
