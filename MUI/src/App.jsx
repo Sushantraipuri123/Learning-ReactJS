6708c84b95749f095f4f54b1b13dee101e7f1be0
Sushant <cashu853@gmail.com>
2024-04-10 09:31:29 +0530

    Learning MUI

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -91,15 +91,10 @@ function App() {
       <h1>MUI Slider</h1>
 
       <Box sx={{ width: 300 }}>
-      <Slider
-        size="small"
-        defaultValue={70}
-        aria-label="Small"
-        valueLabelDisplay="auto"
-      />
+       
       <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
     </Box>
-    
+
     </>
   )
 }
